f3d4bf68a582a8bf5a97ee104bcdb49979ebdc71
koriokeshola <148779548+koriokeshola@users.noreply.github.com>
2025-04-21 16:33:22 +0100

    Create Control.java
    Control class for GUI functionality.

---
+++ Control.java
@@ -0,0 +1,11 @@
+/* Screen: Control for Assignment GUI.
+ * Author: Kori Okeshola
+ * Date: 10th April 2025 */
+
+ public class Control
+{
+    public static void main(String[] args) throws Exception
+    {
+        new Screen("This is my GUI");
+    }
+}
