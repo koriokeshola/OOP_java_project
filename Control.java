96213d0441a348db7e524ca5f075c3e8af3629f1
koriokeshola <148779548+koriokeshola@users.noreply.github.com>
2025-04-29 16:28:41 +0100

    Update Control.java
    Move sample data into control file

--- Control.java
+++ Control.java
@@ -6,6 +6,23 @@
 {
     public static void main(String[] args) throws Exception
     {
-        new Screen("This is my GUI");
+        // Add sample data to frequency table
+        Map<String, Integer> count_1 = new HashMap<>();
+        count_1.put("yes", 1);
+        count_1.put("no", 1);
+        NaiveBayesClassifier.frequencyTable.put("Yes,Stormy,High,No", count_1);
+
+        Map<String, Integer> count_2 = new HashMap<>();
+        count_2.put("yes", 4);
+        count_2.put("no", 1);
+        NaiveBayesClassifier.frequencyTable.put("Yes,Stormy,Low,Yes", count_2);
+
+        Map<String, Integer> count_3 = new HashMap<>();
+        count_3.put("yes", 2);
+        count_3.put("no", 3);
+        NaiveBayesClassifier.frequencyTable.put("No,Clear,Low,No", count_3);
+
+        // Launch GUI
+        SwingUtilities.invokeLater(() -> new PredictionGUI("Prediction GUI"));
     }
 }
